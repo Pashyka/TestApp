e4e41e03067e0b884e951443dbd1fd6aa8846c6e
Pavel <pavelmyyy@gmail.com>
2025-11-14 01:36:20 +0300

    Lesson 3

--- TestApp/ContentView.swift
+++ TestApp/ContentView.swift
@@ -8,21 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var message = "I am a programmer!"
-
+    @State private var text = ""
+    @State private var image = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "swift")
+        VStack{
+            Spacer()
+            Image(image)
                 .resizable()
                 .scaledToFit()
-                .foregroundStyle(.orange)
-            Text(message)
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+                .frame(width: 350, height: 350)
+                .shadow(radius: 10)
+            Text(text)
+                .font(.title)
+                .fontWeight(.black)
                 .foregroundStyle(.red)
-                .fontWeight(.heavy)
-                .font(.largeTitle)
-            Button("Click me!") {
-                message = "Hello!"
+            Spacer()
+            HStack {
+                Button("Click Me") {
+                    let message1 = "Какой-то чувак"
+                    let message2 = "Невъебические парни"
+                    let image1 = "Pashtet"
+                    let image2 = "Vse"
+                    text = (text == message1 ? message2 : message1)
+                    image = (image == image1 ? image2 : image1)
+                }
             }
+            .buttonStyle(.glass)
+            .tint(.purple)
+            .fontWeight(.heavy)
         }
     }
 }
---
+++ TestApp/MyPlayground.playground/Contents.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+let person = "Alice"
+
+//switch person {
+//case "Alice":
+  //  print("Hello, Alice!")
+//case "Pavel":
+  //  print("Hello, Pavel!")
+//default:
+  //  print("Hello, \(person)!")
+    
+//}
+let result = person == "Alice" ? "Hello, Alice!" : "Hello, \(person)!"
+print(result)
