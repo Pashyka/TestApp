9650b677d9b6ba1da9154cf466de8ec79a006803
Pavel <pavelmyyy@gmail.com>
2025-11-14 01:36:20 +0300

    Lesson 3

--- TestApp/ContentView.swift
+++ TestApp/ContentView.swift
@@ -9,20 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var message = "I am a programmer!"
-
+    
     var body: some View {
         VStack {
+            Spacer()
             Image(systemName: "swift")
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
+                .frame(width: 200, height: 200)
             Text(message)
-                .foregroundStyle(.red)
-                .fontWeight(.heavy)
+                .fontWeight(.ultraLight)
                 .font(.largeTitle)
-            Button("Click me!") {
-                message = "Hello!"
+            Spacer()
+            HStack {
+                Button("Awesome!") {
+                    message = "Awesome!"
+                }
+                Button("Great!") {
+                    message = "Great!"
+                }
             }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .tint(.orange)
+            
         }
     }
 }
