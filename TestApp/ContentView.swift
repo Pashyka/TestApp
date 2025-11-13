3c6933f80d0a1fcf5a97f310bda8b9588185ffd9
Pavel <pavelmyyy@gmail.com>
2025-11-14 00:33:29 +0300

    add project

--- TestApp/ContentView.swift
+++ TestApp/ContentView.swift
@@ -8,14 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var imageColor: Color = .orange
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("That is Football to You?")
+                .foregroundStyle(.green)
+                .fontWeight(.thin)
+                .font(.largeTitle)
+            HStack {
+                Image(systemName: "figure.american.football")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundStyle(.blue)
+                Image(systemName: "figure.australian.football")
+                    .resizable()
+                    .foregroundStyle(.indigo)
+                    .scaledToFit()
+                Image(systemName: "figure.soccer")
+                    .resizable()
+                    .foregroundStyle(.purple)
+                    .scaledToFit()
+            }
+            
+
         }
-        .padding()
     }
 }
 
